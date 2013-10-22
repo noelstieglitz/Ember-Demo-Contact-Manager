24705d1173d4f557e87ac47b3b39787b617bad88
Noel Stieglitz <noel@componotech.com>
2013-10-21 20:00:07 -0400

    update view + bootstrap

--- js/app.js
+++ js/app.js
@@ -19,7 +19,9 @@ App.IndexRoute = Em.Route.extend({
 //routers
 App.Router.map(function(){ //map URLs to templates
    this.resource('contacts',{path: '/contacts'}, function(){
-       this.resource('contact', {path: ':contact_id'});
+       this.resource('contact', {path: ':contact_id'}, function(){
+           this.route('edit');
+       });
    }); //maps to /#/contacts
     //define all other URLs in application
 });
@@ -27,10 +29,6 @@ App.Router.map(function(){ //map URLs to templates
 App.ContactsRoute = Em.Route.extend({
     model: function(){
         return this.store.find(App.Contact);
-    },
-    contactCount: function(){
-        debugger;
-        return this.model.length;
     }
 });
 
@@ -39,9 +37,65 @@ App.ContactRoute = Em.Route.extend({
         return this.store.find(App.Contact, params.contact_id);
     }
 });
+
+App.EditContactRoute = Em.Route.extend({
+    model: function() {
+        return this.modelFor('contact');
+    }
+});
+
 //controllers
 App.ContactsController =  Em.ArrayController.extend();
-//App.ContactController = Em.ObjectController.extend();
+
+App.ContactController = Em.ObjectController.extend({
+    isEditing: false,
+
+    editContact: function () {
+        debugger;
+        this.set('isEditing', true);
+        this.transitionToRoute('edit');
+    },
+
+    deleteContact: function (something) {
+        debugger;
+        /*
+        var contact = this.get('model');
+        this.store.deleteRecord(App.Contact, contact);
+        this.get('store').deleteRecord(App.Contact.find(1));
+        */
+        this.transitionToRoute('contacts');
+    }
+});
+
+
+
+//views
+App.EditContactView = Em.TextField.extend({
+    classNames: ['edit'],
+
+    valueBinding: 'todo.title',
+
+    change: function () {
+        var value = this.get('value');
+
+        if (Em.isEmpty(value)) {
+            this.get('controller').removeContact();
+        }
+    },
+
+    focusOut: function () {
+        this.set('controller.isEditing', false);
+    },
+
+    insertNewline: function () {
+        this.set('controller.isEditing', false);
+    },
+
+    didInsertElement: function () {
+        this.$().focus();
+    }
+});
+
 
 //models
 //setup the store.  we are using the fixture adapter for testing
